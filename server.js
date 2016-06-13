399d5db396b911d9a5b56b181925ef540130d825
nikhilGitrepo <nikhilgh5@gmail.com>
2016-06-13 14:11:36 -0400

    tut 16

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+
+var User = require('./models/user');
 
 var app = express();
 
+mongoose.connect('mongodb://root:welcome123@ds011278.mlab.com:11278/ecommerce',function(err){
+  if(err){
+      console.log(err);
+  } else{
+      console.log("Connected to the database");
+  }
+
+});
 
 //Middleware
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true}));
+
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
 app.get('/', function(req,res){
 res.json('welcome nikhil');
+});
+
+app.post('/signup', function(req,res,next){
+   var user = new User();
+   user.profile.name = req.body.name;
+   user.password = req.body.password;
+   user.email = req.body.email;
+
+   user.save(function(err){
+      if(err) return next(err);
+
+       res.json('Successfully created a new user');
+   });
 });
 
 app.listen(3000,function(err){
