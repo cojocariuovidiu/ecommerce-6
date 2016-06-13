b5942ca6d145ea3321ea58ab6e41475c6c281f0c
nikhilGitrepo <nikhilgh5@gmail.com>
2016-06-13 13:14:51 -0400

    commit 2

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var morgan = require('morgan');
+
 var app = express();
+
+
+//Middleware
+app.use(morgan('dev'));
+app.get('/', function(req,res){
+res.json('welcome nikhil');
+});
 
 app.listen(3000,function(err){
     if(err) throw err;
-    console.log('Server is Running');
+    console.log('Server is Running at port 3000');
 });
